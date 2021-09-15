ae146be64cd765a8468842444dbeaf831e3b0643
Lavi26 <89968738+Lavi26@users.noreply.github.com>
2021-09-15 16:21:20 +0530

    Update ReadFileByLine.java

--- JavaAssignment-15Sep/iostream/ReadFileByLine.java
+++ JavaAssignment-15Sep/iostream/ReadFileByLine.java
@@ -9,7 +9,7 @@ public class ReadFileByLine {
 	public static void main(String args[]) throws IOException {
 		Reader file=new FileReader("D:/IO/xyz.txt");
 		BufferedReader bi=new BufferedReader(file);
-		//BufferedReader bi1=null;
+		
 		String i="";
 		while((i = bi.readLine())!=null) {
 			System.out.println(i);
